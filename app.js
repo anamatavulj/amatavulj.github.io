282f15e9b8f7545b4e49f2c37ceea64622acdd3c
anamatavulj <132695248+anamatavulj@users.noreply.github.com>
2023-05-30 11:41:59 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const menu  = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+
+
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top")
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden")
+  } else {
+    backToTopButton.classList.add("hidden")
+  }
+})
+
+const goToTop = () => {
+    document.body.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
+backToTopButton.addEventListener("click", goToTop)
+
+
